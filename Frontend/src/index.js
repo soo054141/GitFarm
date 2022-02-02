2267766f978ce553aa9310924b284764a820e350
sjy <stseo012@gmail.com>
2022-02-03 00:15:25 +0900

    Add GlobalStyle to index

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>,
   document.getElementById("root"),
