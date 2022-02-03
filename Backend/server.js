fd3fecc20ddd7a8daf4a1a55357816de63cc3bba
YWTechIT <abcd8637@naver.com>
2022-02-03 21:34:35 +0900

    Add GitHub OAuth strategy

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import passport from "passport";
 import httpError from "http-errors";
 import connectDB from "./config/db.js";
+import initPassportStrategy from "./passport/index.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
+initPassportStrategy(passport);
 
 app.use((req, res, next) => {
   next(httpError(404).message);
