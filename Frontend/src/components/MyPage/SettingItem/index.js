e6a729ea11cf7919d684a132523963f7f8d2a94f
Wonjun <dittoavec@gmail.com>
2022-02-08 12:03:54 +0900

    Add Connecting modal component

--- Frontend/src/components/MyPage/SettingItem/index.js
+++ Frontend/src/components/MyPage/SettingItem/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Wrapper, SubTitle, Content } from "./style";
 
-export function SettingItem({ subTitle, textColor, content }) {
+export function SettingItem({ subTitle, textColor, content, onClick }) {
   return (
-    <Wrapper>
+    <Wrapper onClick={onClick}>
       <SubTitle textColor={textColor}>{subTitle}</SubTitle>
       <Content>{content}</Content>
     </Wrapper>
