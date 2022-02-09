e2d0bb35cf26c895f7f6278c8cbc22bcbddd8bc8
YWTechIT <abcd8637@naver.com>
2022-02-10 06:27:55 +0900

    Add users routes

---
+++ Backend/routes/api/users.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/extensions */
+import express from "express";
+import passport from "passport";
+import { getTodayTotalCommitAllRepo } from "../../lib/api/GitHub/getTodayTotalCommitAllRepo.js";
+import { getTotalCommitAllRepo } from "../../lib/api/GitHub/getTotalCommitAllRepo.js";
+
+const router = express.Router();
+
+export default (app) => {
+  app.use(
+    "/users",
+    passport.authenticate("jwt", {
+      session: false,
+      failureRedirect: "/api/auth/github",
+    }),
+    router,
+  );
+
+  // @route GET api/users/commits/total
+  // @desc total commits
+  // @access Private
+  router.get("/commits/total", async (req, res) => {
+    const { user } = req;
+    const result = await getTotalCommitAllRepo(user);
+    res.json({ totalCommit: result });
+  });
+
+  // @route GET api/users/commits/today
+  // @desc today total commits
+  // @access Private
+  router.get("/commits/today", async (req, res) => {
+    const { user } = req;
+    const result = await getTodayTotalCommitAllRepo(user);
+    res.json({ todayCommit: result });
+  });
+};
