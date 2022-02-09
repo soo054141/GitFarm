c4c7a5264f758edc9cac3d94b611e3af21dbe8c8
Wonjun <dittoavec@gmail.com>
2022-02-09 14:22:06 +0900

    Add line graph component

--- Frontend/src/pages/graph/index.js
+++ Frontend/src/pages/graph/index.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import { Container } from "@/components/Container/style";
 import { DateController } from "@/components/DateController";
+import { LineGraph } from "./LineGraph";
 import { MonthYearBtn } from "./MonthYearBtn";
 import { PieChartComponent } from "./PieChart";
 
 export function Graph() {
   const [clickButton, setClickButton] = useState(true);
+  const [graphTitle, setGraphTitle] = useState("월간");
+  const [checkMonth, setCheckMonth] = useState(false);
 
   const handleClickButton = () => {
     setClickButton((current) => !current);
+    setCheckMonth((current) => !current);
+    setGraphTitle(clickButton ? "연간" : "월간");
   };
+
   return (
     <Container>
-      <DateController></DateController>
+      {checkMonth && <DateController></DateController>}
       <MonthYearBtn isClick={clickButton} onClick={handleClickButton} />
+      <LineGraph graphTitle={graphTitle} />
       <PieChartComponent />
     </Container>
   );
