b21922637218fdf0dfaefa96e17b3d4a97a9fc8d
YWTechIT <abcd8637@naver.com>
2022-02-10 06:27:41 +0900

    Add getTodayTotalCommit

---
+++ Backend/lib/api/GitHub/getTodayTotalCommitAllRepo.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+import { getAllRepoName } from "../Octokit/utils.js";
+import { getTodayTotalCommitEachRepo } from "./getTodayTotalCommitEachRepo.js";
+
+export const getTodayTotalCommitAllRepo = async (user) => {
+  const repoName = await getAllRepoName(user);
+  const status = await Promise.allSettled(
+    repoName.map((name) => {
+      const commit = getTodayTotalCommitEachRepo(user, name);
+      // commit
+      //   .then((res) => {
+      //     console.log(`repo: ${name}, commit: ${res}`);
+      //   })
+      //   .catch((err) =>
+      //     console.log(`err repo= ${name}, err message = ${err.message}`),
+      //   );
+      return commit;
+    }),
+  );
+
+  const fulfilledValue = status
+    .filter((result) => result.status === "fulfilled")
+    .map((res) => res.value);
+
+  const totalCommit = fulfilledValue.reduce((acc, cur) => acc + cur);
+  return totalCommit;
+};
---
+++ Backend/lib/api/GitHub/getTodayTotalCommitEachRepo.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+import { todaySince, todayUntil } from "../../../utils/date.js";
+import { getOctokitAuth } from "../Octokit/utils.js";
+
+export const getTodayTotalCommitEachRepo = async (
+  user,
+  repo = "alice-sw-engineer-track",
+) => {
+  // const { username } = user;
+  const username = "YWTechIT";
+  const octokit = getOctokitAuth(user);
+  const commitNum = await octokit.paginate(
+    `GET /repos/${username}/${repo}/commits`,
+    {
+      since: todaySince,
+      until: todayUntil,
+      author: username,
+      per_page: 100,
+    },
+    (response) => response.data.length,
+  );
+  return commitNum.reduce((acc, cur) => acc + cur);
+};
