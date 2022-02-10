cebcf64eeeb8ce11c1cc9e8935ca84031a259efa
YWTechIT <abcd8637@naver.com>
2022-02-10 21:23:20 +0900

    Update routes/api services

--- Backend/routes/api/users.js
+++ Backend/routes/api/users.js
@@ -6,6 +6,7 @@ import { getDetailTotalCommitAllRepo } from "../../lib/api/GitHub/commits/today/
 import { getTodayTotalCommitAllRepo } from "../../lib/api/GitHub/commits/today/getTodayTotalCommitAllRepo.js";
 import { getTotalCommitAllRepo } from "../../lib/api/GitHub/commits/total/getTotalCommitAllRepo.js";
 import { getMonthTotalCommitAllRepo } from "../../lib/api/GitHub/commits/year/index.js";
+import { User, Commit } from "../../model/index.js";
 
 const router = express.Router();
 
@@ -19,16 +20,73 @@ export default (app) => {
     router,
   );
 
+  // @route GET api/users/commits/test
+  // @desc test commits
+  // @access Private
+  router.get("/commits/test", async (req, res) => {
+    const { user } = req;
+    const { id } = user;
+    const [{ _id }] = await User.find({ id });
+    try {
+      const result = "678";
+
+      await Commit.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            author: _id,
+            test: result,
+          },
+        },
+        { upsert: true },
+      ).populate({ path: "author" });
+
+      res.json({
+        success: true,
+        message: result,
+      });
+    } catch (err) {
+      const loadDB = await Commit.find({ id: _id });
+      res.json({
+        success: false,
+        message: loadDB,
+      });
+    }
+  });
+
   // @route GET api/users/commits/total
   // @desc total commits
   // @access Private
   router.get("/commits/total", async (req, res) => {
     const { user } = req;
-    const result = await getTotalCommitAllRepo(user);
-    res.json({
-      success: true,
-      totalCommit: result,
-    });
+    const { id } = user;
+    const [{ _id }] = await User.find({ id });
+    try {
+      const result = await getTotalCommitAllRepo(user);
+
+      await Commit.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            author: _id,
+            total: result,
+          },
+        },
+        { upsert: true },
+      ).populate({ path: "author" });
+
+      res.json({
+        success: true,
+        message: result,
+      });
+    } catch (err) {
+      const key = "total";
+      const loadDB = await Commit.find({ id: _id });
+      res.json({
+        success: false,
+        message: loadDB[key],
+      });
+    }
   });
 
   // @route GET api/users/commits/today
@@ -36,50 +94,116 @@ export default (app) => {
   // @access Private
   router.get("/commits/today", async (req, res) => {
     const { user } = req;
-    const result = await getTodayTotalCommitAllRepo(user);
+    const { id } = user;
+    const [{ _id }] = await User.find({ id });
+
+    try {
+      const result = await getTodayTotalCommitAllRepo(user);
+      await Commit.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            author: _id,
+            today: result,
+          },
+        },
+        { upsert: true },
+      ).populate({ path: "author" });
+
+      res.json({
+        success: true,
+        message: result,
+      });
+    } catch (err) {
+      const key = "today";
+      const loadDB = await Commit.find({ id: _id });
+      res.json({
+        success: false,
+        message: loadDB[key],
+      });
+    }
+  });
+
+  // @route GET api/users/commits/today/detail
+  // @desc today detail commits
+  // @access Private
+  router.get("/commits/today/detail", async (req, res) => {
+    const { user } = req;
+    const { id } = user;
+    const [{ _id }] = await User.find({ id });
+
+    try {
+      const result = await getDetailTotalCommitAllRepo(user);
+      await Commit.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            author: _id,
+            todayDetail: result,
+          },
+        },
+        { upsert: true },
+      ).populate({ path: "author" });
+
+      res.json({
+        success: true,
+        message: result,
+      });
+    } catch (err) {
+      const key = "todayDetail";
+      const loadDB = await Commit.find({ id: _id });
+      res.json({
+        success: false,
+        message: loadDB[key],
+      });
+    }
+  });
+
+  // @route GET api/users/languages
+  // @desc user별 languages
+  // @access Private
+  router.get("/languages", async (req, res) => {
+    const { user } = req;
+    const { id } = user;
+    const [{ _id }] = await User.find({ id });
+
+    try {
+      const result = await getLanguagesData(user);
+      await Commit.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            author: _id,
+            languages: result,
+          },
+        },
+        { upsert: true },
+      ).populate({ path: "author" });
+
+      res.json({
+        success: true,
+        message: result,
+      });
+    } catch (err) {
+      const key = "languages";
+      const loadDB = await Commit.find({ id: _id });
+      res.json({
+        success: false,
+        message: loadDB[key],
+      });
+    }
+  });
+
+  // @route GET api/users/commits/total/year/:year
+  // @desc params 기준 월 별 총 commits
+  // @access Private
+  router.get("/commits/total/year/:year", async (req, res) => {
+    const { user, params } = req;
+    const { year } = params;
+    const result = await getMonthTotalCommitAllRepo(user, year);
     res.json({
       success: true,
-      todayCommit: result,
+      message: "연도별 커밋을 보여줍니다.",
     });
   });
 };
-
-// @route GET api/users/commits/detail
-// @desc today detail commits
-// @access Private
-router.get("/commits/today/detail", async (req, res) => {
-  const { user } = req;
-  const result = await getDetailTotalCommitAllRepo(user);
-
-  res.json({
-    success: true,
-    todayDetailCommit: result,
-  });
-});
-
-// @route GET api/users/languages
-// @desc user별 languages
-// @access Private
-router.get("/languages", async (req, res) => {
-  const { user } = req;
-  const result = await getLanguagesData(user);
-
-  res.json({
-    success: true,
-    data: result,
-  });
-});
-
-// @route GET api/users/commits/total/year/:year
-// @desc params 기준 월 별 총 commits
-// @access Private
-router.get("/commits/total/year/:year", async (req, res) => {
-  const { user, params } = req;
-  const { year } = params;
-  const result = await getMonthTotalCommitAllRepo(user, year);
-  console.log(result);
-  res.json({
-    success: true,
-    message: "연도별 커밋을 보여줍니다.",
-  });
-});
