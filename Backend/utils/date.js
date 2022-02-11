bdde5f9f5a059331dfde1259d4c76d1503f04ab6
YWTechIT <abcd8637@naver.com>
2022-02-12 03:27:11 +0900

    Add util date functions isLeapYear, getCalendar

--- Backend/utils/date.js
+++ Backend/utils/date.js
@@ -21,16 +21,33 @@ export const endOfDay = "T23:59:59Z";
 export const todaySince = `${today}${startOfDay}`;
 export const todayUntil = `${today}${endOfDay}`;
 // NOTE: test때는 두번째 monthDays 사용하기(API rate limit 방지)
-// export const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-export const monthDays = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-export const repoName = [
-  "ci-cd-practice",
-  "commento-assignment",
-  "starbucks-recipe-project",
-];
+export const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// export const monthDays = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+export const repoName = ["ci-cd-practice", "commento-assignment"];
 export const monthPerYear = 12;
 
 export function fillZero(target, targetLenth, padString) {
   const str = target.toString();
   return str.padStart(targetLenth, padString);
 }
+
+const isLeapYear = (currentYear) =>
+  (currentYear % 4 === 0 && currentYear % 100 !== 0) || currentYear % 400 === 0;
+
+const getMonth = () => {
+  const checkLeapYear = isLeapYear(year);
+
+  return checkLeapYear ? [31, 29, 31, 30] : [31, 28];
+};
+
+export const getCalendar = () => {
+  const calendar = [];
+  const monthes = getMonth();
+
+  monthes.forEach((item) => {
+    const filldays = Array.from({ length: item }, () => 0);
+    calendar.push(filldays);
+  });
+
+  return calendar;
+};
