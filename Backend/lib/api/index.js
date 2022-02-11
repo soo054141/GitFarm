6cb8560809e40fac650c57d4ed72a0a13a24fa9a
YWTechIT <abcd8637@naver.com>
2022-02-11 17:42:42 +0900

    Move folder lib/api/GitHub/total to lib/api/GitHub/repo

--- Backend/lib/api/index.js
+++ Backend/lib/api/index.js
@@ -2,8 +2,8 @@
 /* eslint-disable import/prefer-default-export */
 export { getTodayTotalCommitAllRepo } from "./GitHub/commits/today/getTodayTotalCommitAllRepo.js";
 export { getDetailTotalCommitAllRepo } from "./GitHub/commits/today/detail/getDetailTotalRepo.js";
-export { getTotalCommitAllRepo } from "./GitHub/commits/total/getTotalCommitAllRepo.js";
+export { getTotalCommitAllRepo } from "./GitHub/repos/total/getTotalCommitAllRepo.js";
 export { getMonthTotalCommitAllRepo } from "./GitHub/commits/per/index.js";
-export { getLanguagesData } from "./GitHub/language/index.js";
+export { getLanguagesData } from "./GitHub/repos/language/index.js";
 export { getContinuousCommitAllRepo } from "./GitHub/commits/continuous/getContinuousCommitAllRepo.js";
 export { getRecentYearTotalCommit } from "./GitHub/commits/recent/index.js";
