62f4d1a7cdd1d25b0c907e43b1a059d2b600f6b1
kimyurim <soo054141@gmail.com>
2022-02-12 17:38:40 +0900

    Add mock for API 연결

--- Frontend/src/pages/myPage/CommitCount/index.js
+++ Frontend/src/pages/myPage/CommitCount/index.js
@@ -1,20 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import * as CommitInfo from "./style";
 
-function CommitCount() {
+function CommitCount({ total, continues }) {
   return (
     <CommitInfo.Wrapper>
       <CommitInfo.Counter>
         <CommitInfo.Title>Total</CommitInfo.Title>
-        <CommitInfo.Count>4,116</CommitInfo.Count>
+        <CommitInfo.Count>{total.toLocaleString()}</CommitInfo.Count>
       </CommitInfo.Counter>
       <CommitInfo.Divider />
       <CommitInfo.Counter>
         <CommitInfo.Title>Longest</CommitInfo.Title>
-        <CommitInfo.Count>43 days</CommitInfo.Count>
+        <CommitInfo.Count>{continues} days</CommitInfo.Count>
       </CommitInfo.Counter>
     </CommitInfo.Wrapper>
   );
 }
+
+CommitCount.propTypes = {
+  total: PropTypes.number.isRequired,
+  continues: PropTypes.number.isRequired,
+};
 
 export default CommitCount;
