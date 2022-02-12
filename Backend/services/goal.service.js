52e23f603c69ac3b8da620d5bffeb93fb4c1903a
YWTechIT <abcd8637@naver.com>
2022-02-13 02:42:49 +0900

    Add goal.service.js

--- Backend/services/goal.service.js
+++ Backend/services/goal.service.js
@@ -2,6 +2,16 @@
 /* eslint-disable import/prefer-default-export */
 import { User } from "../model/index.js";
 
+export const getGoal = async (req) => {
+  const { user } = req;
+  const { id } = user;
+  const [{ _id }] = await User.find({ id });
+
+  const userDocument = await User.findById(_id);
+  const { goal } = userDocument;
+  return goal;
+};
+
 export const setGoal = async (req) => {
   const { user } = req;
   const { goal } = req.body;
