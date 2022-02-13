1c74bfb85d9e8eb863a08f85f6183fbb8f32f1b0
sjy <stseo012@gmail.com>
2022-02-13 12:46:07 +0900

    Add calendar component api

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -2,11 +2,41 @@ import axios from "axios";
 
 const url = "http://localhost:8888";
 
+// test
 export const getBreeds = async () => {
   try {
     const r = await axios.get(`${url}/posts`);
     console.log(r);
   } catch (error) {
     console.error(error);
+  }
+};
+
+// mypage
+export const getMyInfo = async () => {
+  try {
+    const res = await axios.get(`${url}/mypage`);
+    return res.data;
+  } catch (error) {
+    console.error();
+  }
+};
+
+// graph - 월간
+export const getCommitsTotalPerMonth = async (year) => {
+  try {
+    const res = await axios.get(`${url}/month/${year}`);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+export const getCommitMonthly = async () => {
+  try {
+    const res = await axios.get(`${url}/perMonth`);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    return error;
   }
 };
