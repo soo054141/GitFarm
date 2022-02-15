7fc655710ec3d9164c31f71edbc15b3424ae2afb
YWTechIT <abcd8637@naver.com>
2022-02-15 21:41:36 +0900

    Refactor users.controller.js: 함수 모듈화

--- Backend/controller/levels.controller.js
+++ Backend/controller/levels.controller.js
@@ -1,6 +1,6 @@
+/* eslint-disable import/extensions */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable import/prefer-default-export */
-/* eslint-disable import/extensions */
 import { Level } from "../model/index.js";
 import {
   getPullsAllRepo,
@@ -12,12 +12,25 @@ import {
   FindValueByKey,
   FindByIdAndUpdate,
 } from "../services/index.js";
-import { getUserObjectId } from "../utils/db.js";
+import { getUpdatedAtById, getUserObjectId } from "../utils/db.js";
 import { ViewResponseJSON } from "./view.controller.js";
+import { isInTime, TARGET_TIME } from "../utils/date.js";
 
 export const getLevelsController = async (req, res) => {
   const { user } = req;
   const _id = await getUserObjectId(user);
+  const updatedAt = await getUpdatedAtById(user, Level);
+  const inTime = isInTime(TARGET_TIME, updatedAt);
+  if (inTime) {
+    const commits = await FindValueByKey(Level, _id, "commits");
+    const issues = await FindValueByKey(Level, _id, "issues");
+    const pulls = await FindValueByKey(Level, _id, "pulls");
+    const score = await FindValueByKey(Level, _id, "score");
+    const result = { score, commits, issues, pulls };
+    ViewResponseJSON(res, false, "data", result);
+    return;
+  }
+
   try {
     const commits = await getCommitsAllRepo(user);
     const issues = await getIssuesAllRepo(user);
@@ -43,6 +56,14 @@ export const getLevelsController = async (req, res) => {
 export const getLevelsCommitsController = async (req, res) => {
   const { user } = req;
   const _id = await getUserObjectId(user);
+  const updatedAt = await getUpdatedAtById(user, Level);
+  const inTime = isInTime(TARGET_TIME, updatedAt);
+  if (inTime) {
+    const result = await FindValueByKey(Level, _id, "commits");
+    ViewResponseJSON(res, true, "commits", result);
+    return;
+  }
+
   try {
     const result = await getCommitsAllRepo(user);
     await FindByIdAndUpdate(Level, _id, "commits", result);
@@ -56,6 +77,14 @@ export const getLevelsCommitsController = async (req, res) => {
 export const getLevelsIssuesController = async (req, res) => {
   const { user } = req;
   const _id = await getUserObjectId(user);
+  const updatedAt = await getUpdatedAtById(user, Level);
+  const inTime = isInTime(TARGET_TIME, updatedAt);
+  if (inTime) {
+    const result = await FindValueByKey(Level, _id, "issues");
+    ViewResponseJSON(res, true, "issues", result);
+    return;
+  }
+
   try {
     const result = await getIssuesAllRepo(user);
     await FindByIdAndUpdate(Level, _id, "issues", result);
@@ -69,6 +98,14 @@ export const getLevelsIssuesController = async (req, res) => {
 export const getLevelsPullsController = async (req, res) => {
   const { user } = req;
   const _id = await getUserObjectId(user);
+  const updatedAt = await getUpdatedAtById(user, Level);
+  const inTime = isInTime(TARGET_TIME, updatedAt);
+  if (inTime) {
+    const result = await FindValueByKey(Level, _id, "pulls");
+    ViewResponseJSON(res, true, "pulls", result);
+    return;
+  }
+
   try {
     const result = await getPullsAllRepo(user);
     await FindByIdAndUpdate(Level, _id, "pulls", result);
