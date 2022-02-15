7463234ba8c15c7fa64cd498df1570b73cde7387
Jin1won <wlsdnjs16521@gmail.com>
2022-02-15 13:55:22 +0900

    Add today issues

---
+++ Backend/lib/api/GitHub/commits/today/issues/getTodayIssueAllRepo.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
+import { getAllRepoName } from "../../../Octokit/utils.js";
+import { getTodayIssueEachRepo } from "./getTodayIssueEachRepo.js";
+
+export const getTodayIssueAllRepo = async (user) => {
+  const repoName = await getAllRepoName(user);
+  const status = await Promise.allSettled(
+    repoName.map((name) => {
+      const issues = getTodayIssueEachRepo(user, name);
+      return issues;
+    }),
+  );
+
+  const fulfilledValue = status
+    .filter((result) => result.status === "fulfilled")
+    .map((res) => res.value);
+
+  const todayIssues = fulfilledValue.reduce((acc, cur) => acc + cur);
+
+  return todayIssues;
+};
