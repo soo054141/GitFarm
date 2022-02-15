680c9e562155b8727a379aacfa1b7efe5061697a
Wonjun <dittoavec@gmail.com>
2022-02-15 14:56:21 +0900

    Update db, api

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -52,6 +52,48 @@ export const getUserBadges = async () => {
   }
 };
 
+export const getGoal = async () => {
+  try {
+    const res = await axios.get(`${url}/goal`);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getResolution = async () => {
+  try {
+    const res = await axios.get(`${url}/resolution`);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const postGoal = async (goalNum) => {
+  try {
+    const res = await axios.post(`${url}/goal`, {
+      success: true,
+      goal: goalNum,
+    });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const postResolution = async (resolutionString) => {
+  try {
+    const res = await axios.post(`${url}/resolution`, {
+      success: true,
+      resolution: resolutionString,
+    });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
 // 총 커밋 개수 호출(getFullYear 기준 최근 3년)
 export const getRecentThreeYear = async () => {
   try {
