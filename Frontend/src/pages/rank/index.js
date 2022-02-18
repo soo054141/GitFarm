c8050274ccd55cb9db146f3fb16e640712e8e486
Wonjun <dittoavec@gmail.com>
2022-02-19 04:33:23 +0900

    Add Skeleton UI

--- Frontend/src/pages/rank/index.js
+++ Frontend/src/pages/rank/index.js
@@ -4,18 +4,22 @@ import * as api from "@/api";
 import RankTitle from "./RankTitle";
 import Rank from "./Rank";
 import * as Ranks from "./style";
+import Skeletons from "./Skeleton";
 
 function RankPage() {
   const [myRank, setMyRank] = useState({});
   const [userRank, setUserRank] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getRank = async () => {
+    setLoading(true);
     const rankData = await api.getRank();
 
     if (rankData.success) {
       setMyRank(rankData.data.myRank);
       setUserRank(rankData.data.userRank);
     }
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -26,24 +30,37 @@ function RankPage() {
     <Container>
       <RankTitle />
       <Ranks.ResponsiveDiv>
-        <Rank
-          myRanking
-          imgURL={myRank.avatarUrl}
-          id={myRank.username}
-          point={myRank.totalScore}
-        />
-        <Ranks.ResponsivUserRankWrapper>
-          {userRank.map((it) => (
-            <Rank
-              key={`${it.username}-${it.rank}-${it.totalScore}`}
-              ranking={it.rank}
-              imgURL={it.avatarUrl}
-              id={it.username}
-              point={it.totalScore}
-              rank={it.rank}
-            />
-          ))}
-        </Ranks.ResponsivUserRankWrapper>
+        {loading ? (
+          <Skeletons />
+        ) : (
+          <Rank
+            myRanking
+            imgURL={myRank.avatarUrl}
+            id={myRank.username}
+            point={myRank.totalScore}
+          />
+        )}
+
+        {loading ? (
+          <Ranks.ResponsivUserRankWrapper>
+            <Skeletons />
+            <Skeletons />
+            <Skeletons />
+          </Ranks.ResponsivUserRankWrapper>
+        ) : (
+          <Ranks.ResponsivUserRankWrapper>
+            {userRank.map((it) => (
+              <Rank
+                key={`${it.username}-${it.rank}-${it.totalScore}`}
+                ranking={it.rank}
+                imgURL={it.avatarUrl}
+                id={it.username}
+                point={it.totalScore}
+                rank={it.rank}
+              />
+            ))}
+          </Ranks.ResponsivUserRankWrapper>
+        )}
       </Ranks.ResponsiveDiv>
     </Container>
   );
