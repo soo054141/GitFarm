a4c387e177d86ffb2d6b2a212ad857ac49619be0
YWTechIT <abcd8637@naver.com>
2022-02-18 11:53:19 +0900

    Update timeLimit middleware.js

--- Backend/middleware/time-limit.js
+++ Backend/middleware/time-limit.js
@@ -2,18 +2,26 @@ import { TARGET_TIME, ONE_MILLISECOND, ONE_MINUTE } from "../utils/date.js";
 import { Commit } from "../model/index.js";
 import { getUpdatedAtById } from "../utils/db.js";
 
-export default (app) => {
-  // eslint-disable-next-line no-unused-vars
-  app.use(async (err, req, res, next) => {
-    const { user } = req;
+export default async (req, res, next) => {
+  const { user } = req;
+
+  try {
     const updatedAt = await getUpdatedAtById(user, Commit);
+
+    // commit 정보가 없을 때
+    if (!updatedAt) {
+      return next();
+    }
+
     const calc = Math.floor(
       (new Date() - updatedAt) / (ONE_MILLISECOND * ONE_MINUTE),
     );
-    if (calc >= TARGET_TIME) {
-      next();
-    }
 
-    res.redirect("/");
-  });
+    if (calc >= TARGET_TIME) {
+      return next();
+    }
+    return res.status(200).redirect("/");
+  } catch (err) {
+    next(err);
+  }
 };
