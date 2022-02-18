2ade8e1995ec17a1ba5305533ac7e0d588821bde
Wonjun <dittoavec@gmail.com>
2022-02-18 16:07:44 +0900

    Fix 로그인 계정 id 표시, 커서 css 변경

--- Frontend/src/pages/myPage/SettingItem/index.js
+++ Frontend/src/pages/myPage/SettingItem/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Wrapper, SubTitle, Content } from "./style";
 
-function SettingItem({ subTitle, textColor, content, onClick }) {
+function SettingItem({ subTitle, textColor, content, onClick, cursor }) {
   return (
-    <Wrapper onClick={onClick}>
+    <Wrapper onClick={onClick} cursor={cursor}>
       <SubTitle textColor={textColor}>{subTitle}</SubTitle>
       <Content>{content}</Content>
     </Wrapper>
@@ -15,13 +15,16 @@ SettingItem.defaultProps = {
   subTitle: "로그인 계정22",
   textColor: "#595959",
   content: "testId",
+  cursor: false,
+  onClick: () => {},
 };
 
 SettingItem.propTypes = {
   subTitle: PropTypes.string,
   textColor: PropTypes.string,
   content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  // onClick: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]).isRequired,
+  onClick: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
+  cursor: PropTypes.bool,
 };
 
 export default SettingItem;
