e0583ef939c4d2f1677b97cc9e133d0adf0cbb2f
Wonjun <dittoavec@gmail.com>
2022-02-19 15:07:17 +0900

    Fix graph component css

--- Frontend/src/pages/graph/LineGraph/style.js
+++ Frontend/src/pages/graph/LineGraph/style.js
@@ -13,18 +13,14 @@ export const Container = styled.div`
 
 export const Wrapper = styled.div`
   position: absolute;
-  top: 48px;
+  top: 63px;
   right: 20px;
 `;
 
 export const Title = styled.h5`
   font-size: 13.55px;
-  margin-left: 18px;
-  margin-top: 15px;
-  padding-top: 17px;
-  @media ${({ theme }) => theme.device.laptop} {
-    padding-top: 0;
-  }
+  margin-left: 24px;
+  padding-top: 26px;
 `;
 
 export const NoData = styled.p`
@@ -36,3 +32,5 @@ export const NoData = styled.p`
   font-size: 15px;
   color: ${(props) => props.theme.darkGray};
 `;
+
+export const LineGraphContainer = styled.div``;
