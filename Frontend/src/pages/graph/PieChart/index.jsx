aec40317ebeea33c1bd12583f76d2f4a7dbab165
Wonjun <dittoavec@gmail.com>
2022-02-27 03:04:29 +0900

    Simplify ternary operator

--- Frontend/src/pages/graph/PieChart/index.jsx
+++ Frontend/src/pages/graph/PieChart/index.jsx
@@ -12,63 +12,54 @@ function PieChartComponent({ reposLanguage, loading }) {
 
   return (
     <PieCharts.Container>
-      {loading ? (
-        <LoadingModal />
-      ) : (
+      {loading && <LoadingModal />}
+      {!loading && reposLanguage.length && (
         <PieCharts.PieChartContainer>
-          {reposLanguage.length ? (
-            <>
-              <PieCharts.Wrapper>
-                <PieCharts.Heading>
-                  <PieCharts.Title>사용 언어 비율</PieCharts.Title>
-                  <PieCharts.Description>
-                    전체 레포 기준 (상위 5개)
-                  </PieCharts.Description>
-                </PieCharts.Heading>
-                <PieCharts.RatioWrapper>
-                  {languageRatioArray &&
-                    languageRatioArray.map((it, idx) => (
-                      <PieCharts.LangColorBoxWrapper
-                        key={`${it.name}-${it.value}`}
-                      >
-                        <PieCharts.LangColorBox idx={COLORS[idx]} />
-                        <div>
-                          <PieCharts.LangText>{it.value}%</PieCharts.LangText>
-                          <PieCharts.LangText>{it.name}</PieCharts.LangText>
-                        </div>
-                      </PieCharts.LangColorBoxWrapper>
-                    ))}
-                </PieCharts.RatioWrapper>
-              </PieCharts.Wrapper>
+          <PieCharts.Wrapper>
+            <PieCharts.Heading>
+              <PieCharts.Title>사용 언어 비율</PieCharts.Title>
+              <PieCharts.Description>
+                전체 레포 기준 (상위 5개)
+              </PieCharts.Description>
+            </PieCharts.Heading>
+            <PieCharts.RatioWrapper>
+              {languageRatioArray.map((it, idx) => (
+                <PieCharts.LangColorBoxWrapper key={`${it.name}-${it.value}`}>
+                  <PieCharts.LangColorBox idx={COLORS[idx]} />
+                  <div>
+                    <PieCharts.LangText>{it.value}%</PieCharts.LangText>
+                    <PieCharts.LangText>{it.name}</PieCharts.LangText>
+                  </div>
+                </PieCharts.LangColorBoxWrapper>
+              ))}
+            </PieCharts.RatioWrapper>
+          </PieCharts.Wrapper>
 
-              <PieCharts.PieWrapper>
-                {languageRatioArray && (
-                  <PieChart width={200} height={200}>
-                    <Pie
-                      data={languageRatioArray}
-                      cx="50%"
-                      cy="50%"
-                      innerRadius={40}
-                      outerRadius={70}
-                      fill="#8884d8"
-                      dataKey="value"
-                      isAnimationActive={false}
-                    >
-                      {languageRatioArray.map((it, index) => (
-                        <Cell
-                          key={`cell-${it.name}-${it.value}`}
-                          fill={COLORS[index % COLORS.length]}
-                        />
-                      ))}
-                    </Pie>
-                  </PieChart>
-                )}
-              </PieCharts.PieWrapper>
-            </>
-          ) : (
-            <PieCharts.NoData>데이터가 없습니다.</PieCharts.NoData>
-          )}
+          <PieCharts.PieWrapper>
+            <PieChart width={200} height={200}>
+              <Pie
+                data={languageRatioArray}
+                cx="50%"
+                cy="50%"
+                innerRadius={40}
+                outerRadius={70}
+                fill="#8884d8"
+                dataKey="value"
+                isAnimationActive={false}
+              >
+                {languageRatioArray.map((it, index) => (
+                  <Cell
+                    key={`cell-${it.name}-${it.value}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </PieCharts.PieWrapper>
         </PieCharts.PieChartContainer>
+      )}
+      {!loading && !reposLanguage.length && (
+        <PieCharts.NoData>데이터가 없습니다.</PieCharts.NoData>
       )}
     </PieCharts.Container>
   );
