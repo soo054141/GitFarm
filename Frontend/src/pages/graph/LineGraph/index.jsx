b983247356f79e359a6d239dc2966738c93f08a2
Wonjun <dittoavec@gmail.com>
2022-02-26 22:27:58 +0900

    Refactor 삼항연산자 제거

--- Frontend/src/pages/graph/LineGraph/index.jsx
+++ Frontend/src/pages/graph/LineGraph/index.jsx
@@ -14,34 +14,32 @@ import * as LineGraphs from "./style";
 function LineGraph({ commitData, loading }) {
   return (
     <LineGraphs.Container>
-      {loading ? (
-        <LoadingModal />
-      ) : (
-        <LineGraphs.LineGraphContainer>
-          {commitData.length ? (
-            <>
-              <LineGraphs.Title>월간 커밋 추이</LineGraphs.Title>
-              <LineGraphs.Wrapper>
-                <LineChart width={350} height={280} data={commitData}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Line
-                    type="monotone"
-                    dataKey="commit"
-                    stroke="#6ABD8C"
-                    activeDot={{ r: 2 }}
-                    isAnimationActive={false}
-                  />
-                </LineChart>
-              </LineGraphs.Wrapper>
-            </>
-          ) : (
-            <LineGraphs.NoData>데이터가 없습니다.</LineGraphs.NoData>
-          )}
-        </LineGraphs.LineGraphContainer>
-      )}
+      <LineGraphs.LineGraphContainer>
+        {loading && <LoadingModal />}
+        {!loading && commitData.length && (
+          <>
+            <LineGraphs.Title>월간 커밋 추이</LineGraphs.Title>
+            <LineGraphs.Wrapper>
+              <LineChart width={350} height={280} data={commitData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Line
+                  type="monotone"
+                  dataKey="commit"
+                  stroke="#6ABD8C"
+                  activeDot={{ r: 2 }}
+                  isAnimationActive={false}
+                />
+              </LineChart>
+            </LineGraphs.Wrapper>
+          </>
+        )}
+        {!loading && !commitData.length && (
+          <LineGraphs.NoData>데이터가 없습니다.</LineGraphs.NoData>
+        )}
+      </LineGraphs.LineGraphContainer>
     </LineGraphs.Container>
   );
 }
